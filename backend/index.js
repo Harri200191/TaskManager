e9f851f1f54db7165ac85332103414f788cf1cc8
haris rehman <harisrehmanchugtai@gmail.com>
2023-09-11 20:27:05 +0500

    Created a new backend layout

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,17 @@
+// if we write npm start, it runs the index.js file
+// if we write npm run backend it starts nodemon index.js
 const express = require("express");
+const connectDB = require("./config/connectDB");
+const dotenv = require("dotenv");
 
+dotenv.config();
 const app = express();
+connectDB();
 const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, resp) => {
+    resp.send("Home Page");
+});
 
 app.listen(PORT, () => {
     console.log("Server running on port: ", PORT);
