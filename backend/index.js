a029998fcee1b9308293d040a0f60e3c324e2a13
haris rehman <harisrehmanchugtai@gmail.com>
2023-09-12 16:30:24 +0500

    Update

--- backend/index.js
+++ backend/index.js
@@ -9,8 +9,23 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+
 app.get("/", (req, resp) => {
     resp.send("Home Page");
+});
+
+// CREATING A TASK
+app.post("/api/tasks", async (req, resp) => {
+    try{
+        const task = await task_model.create(req.body);
+        resp.status(200).json(task);
+    }
+    catch(error){
+        resp.status(500).json({
+            msg: error.message
+        })
+    };
 });
 
 // To make sure that our database connects before our server does
