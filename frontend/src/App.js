8c5f47fdd95182b09161f12864e6d9010415c717
haris rehman <harisrehmanchugtai@gmail.com>
2023-09-13 15:45:32 +0500

    Update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,8 @@ import Tasklist from "./components/Tasklist";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+export const URL = process.env.REACT_APP_SERVER_URL
+
 function App() {
   return (
     <div className="app">
--- frontend/src/components/Tasklist.jsx
+++ frontend/src/components/Tasklist.jsx
@@ -4,6 +4,8 @@ import Task from './Task';
 import { useState } from 'react';
 import {toast} from 'react-toastify'
 import axios from 'axios'
+import { URL } from '../App';
+
 
 //http://localhost:5000/api/tasks
 
@@ -26,7 +28,7 @@ const Tasklist = () => {
       return toast.error("Input can not be empty")
     }
     try{
-      await axios.post("http://localhost:5000/api/tasks/", formData)
+      await axios.post(`${URL}/api/tasks/`, formData)
       toast.success("Task added succesfully!")
       setformData({...formData, name: ""})
     }
