c0aa8d95df76f2d2eeb096ca40fa3939451500c6
haris rehman <harisrehmanchugtai@gmail.com>
2023-09-13 17:14:49 +0500

    Update

--- frontend/src/components/Taskform.jsx
+++ frontend/src/components/Taskform.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 
 const Taskform = ({createTask, name, handleInputChange, isediting, updateTask}) => {
+
   return (
     <form className='task-form' onSubmit={isediting ?updateTask :createTask}>
-      <input type='Text' placeholder='Add a name' name="name" value ={name} onChange={handleInputChange}/>
-      <button type='submit'>{isediting ? "Edit" : "Select"}</button>
+
+        <input type='Text' placeholder='Add a name' name="name" value ={name} onChange={handleInputChange}/>
+
+        <button type='submit'>
+            {isediting ? "Edit" : "Select"}
+        </button>
+        
     </form>
   )
+
 }
 
 export default Taskform;
