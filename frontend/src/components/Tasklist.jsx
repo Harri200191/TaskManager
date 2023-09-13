fc1fb17af146febf376441b3fe9628e0685e90aa
haris rehman <harisrehmanchugtai@gmail.com>
2023-09-13 15:05:37 +0500

    Update

--- frontend/src/components/Tasklist.jsx
+++ frontend/src/components/Tasklist.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import Taskform from './Taskform'
 import Task from './Task';
+import { useState } from 'react';
 
 const Tasklist = () => {
+  const [formData, setformData] = useState({
+    name: {},
+    completed: false
+  })
+
+  const {name} = formData
+
+  const handleInputChange = (e) => {
+    const {name, value} = e.target
+    setformData({...formData, [name]: value})
+  };
+
+  const createTask = async () => {
+
+  }
+
   return (
     <div>
       <h2 >Name Selecter App</h2>
-      <Taskform/>
+      <Taskform name={name} handleInputChange={handleInputChange} createTask={createTask}/>
       <div className='--flex-between --pb'>
         <p>
           <b>Total Names: </b> 0
