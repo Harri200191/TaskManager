ed7cf749917c0790f9f453f308e5a029e80c2828
haris rehman <harisrehmanchugtai@gmail.com>
2023-09-14 16:13:30 +0500

    Update

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:3000", "https://name-system.onrender.com"]
+}));
+
 app.use(express.urlencoded({extended: false}));
 app.use("/api/tasks", taskroute);
 
