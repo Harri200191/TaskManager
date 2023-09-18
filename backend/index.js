2bc09328c09409823c190770c6e5d47eb71f974b
haris rehman <harisrehmanchugtai@gmail.com>
2023-09-18 23:58:56 +0500

    Update

--- backend/index.js
+++ backend/index.js
@@ -16,8 +16,6 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const io = socketIO(app);
-
 app.use(express.json());
 app.use(cors({
     origin: ["http://localhost:3000", "https://name-system.onrender.com"]
